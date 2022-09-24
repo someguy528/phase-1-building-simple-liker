42ac22736eb60de065e5256b5cef9e35716cccd8
Brandon Castro <castroyou999@gmail.com>
2022-09-23 21:34:59 -0400

    commit tree 84f89a697b4523076191a5f6a5163396dddfc9b5

--- main.js
+++ main.js
@@ -4,8 +4,36 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const hideModal = () => document.getElementById("modal").classList.add("hidden");
+hideModal();
+const likeGlyph = document.getElementsByClassName("like");
+console.log(likeGlyph)
 
-
+for (let element of likeGlyph){element.addEventListener("click", (e) => {
+  console.log(e)
+  console.log(mimicServerCall()
+  .then(data => {
+    console.log(data),
+    console.log(element.classList)
+      if(element.classList[1] !== "activated-heart"){
+      element.classList.add("activated-heart")
+    }
+      else if(element.classList[1] === "activated-heart"){
+      element.classList.remove("activated-heart")
+      }
+  })
+  .catch(error => {
+    document.getElementById("modal").classList.remove("hidden");
+    document.getElementById("modal-message").innerText = `${error}`;
+    setTimeout(hideModal, 3000);
+  })
+  )
+  // return fetch("http://mimicServer.example.com")
+  // .then(response => response.json)
+  // .then(data => console.log(data))
+  
+} )
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
